af2bba47fb389e4a29e8e614109cc834ae77831b
meskatabusayed <meskatabusayed@gmail.com>
2024-09-22 17:09:52 +0530

    single data fetch

--- src/app/(withCommonLayout)/posts/[postId]/page.tsx
+++ src/app/(withCommonLayout)/posts/[postId]/page.tsx
@@ -1,3 +1,5 @@
+import PostDetails from '@/components/ui/PostDetails'
+import { getPost } from '@/services/PostServices'
 import React from 'react'
 
 type TProps = {
@@ -6,10 +8,12 @@ type TProps = {
     }
 }
 
-const PostDetailsPage = ({params} : TProps) => {
+const PostDetailsPage = async({params} : TProps) => {
+    const post =await getPost(params.postId);
+    
   return (
     <div>
-      <h1>Posts Details</h1>
+      <PostDetails post={post}/>
     </div>
   )
 }
